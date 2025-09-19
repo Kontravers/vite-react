25b6a33f949fcee5d3bdf78ee3264dd7d1b75a74
Kontravers <rasterman.vector@gmail.com>
2025-09-19 21:46:31 +0200

    scaling with parent 3

--- src/GeneratedComponent.tsx
+++ src/GeneratedComponent.tsx
@@ -1,30 +1,11 @@
-import { PulsingBorder } from "@paper-design/shaders-react";
+/** @paper-design/shaders-react@0.0.53 */
+import { PulsingBorder } from '@paper-design/shaders-react';
 
+/**
+ * Code exported from Paper
+ * https://app.paper.design/file/01K53FWV94T9HMTSF6T72BC5BN?node=01K5HJJGT050FGH5RRQDW1RKJ5
+ * on Sep 19, 2025 at 9:46 PM.
+ */
 export default function GeneratedComponent() {
-  return (
-    <PulsingBorder
-      colors={["#0dc1fd", "#d915ef", "#ff3f2ecc"]}
-      colorBack="#00000000"
-      speed={1.38}
-      roundness={0.25}
-      thickness={0.05}
-      softness={0}
-      intensity={0.37}
-      bloom={0.97}
-      spots={2}
-      spotSize={0.78}
-      pulse={0.42}
-      smoke={0.57}
-      smokeSize={0}
-      scale={1}
-      rotation={0}
-      style={{
-        display: "block",
-        backgroundColor: "transparent",
-        width: "100%",
-        height: "100%",
-        borderRadius: "37px", // fixed corners
-      }}
-    />
-  );
+  return <PulsingBorder colors={['#0dc1fd', '#d915ef', '#ff3f2ecc']} colorBack="#00000000" speed={1.17} roundness={0} thickness={0} softness={0} intensity={0.37} bloom={0.97} spots={2} spotSize={0.78} pulse={0.42} smoke={0.57} smokeSize={0} scale={1} rotation={0} style={{ borderRadius: '0px', height: '130px', width: '531px' }} />;
 }
