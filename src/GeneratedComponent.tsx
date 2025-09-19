5f77ab8d3be4955daeb116553f4e630bf1100c3c
Kontravers <rasterman.vector@gmail.com>
2025-09-19 22:03:58 +0200

    reworking the SHAAAPE2

--- src/GeneratedComponent.tsx
+++ src/GeneratedComponent.tsx
@@ -1,23 +1,11 @@
+/** @paper-design/shaders-react@0.0.53 */
 import { PulsingBorder } from '@paper-design/shaders-react';
 
-<PulsingBorder
-  height={200}
-  width={400}
-  colors={["#0dc1fd", "#d915ef", "#ff3f2ecc"]}
-  colorBack="#000000"
-  roundness={0.25}
-  thickness={0.2}
-  softness={0.75}
-  intensity={0.2}
-  bloom={0.45}
-  spots={3}
-  spotSize={0.4}
-  pulse={0.5}
-  smoke={0.35}
-  smokeSize={0.6}
-  offsetX={0}
-  offsetY={0}
-  scale={1}
-  rotation={0}
-  speed={1}
-/>
+/**
+ * Code exported from Paper
+ * https://app.paper.design/file/01K53FWV94T9HMTSF6T72BC5BN?node=01K5HJJGT050FGH5RRQDW1RKJ5
+ * on Sep 19, 2025 at 10:03 PM.
+ */
+export default function GeneratedComponent() {
+  return <PulsingBorder colors={['#20A9F1', '#838CD8', '#DE79BC']} colorBack="#00000000" speed={0.25} roundness={0.75} thickness={0} softness={0} intensity={1} bloom={0.04} spots={2} spotSize={0.78} pulse={0.33} smoke={0.49} smokeSize={0.9} scale={1} rotation={0} style={{ borderRadius: '53px', height: '100%', width: '100%' }} />;
+}
