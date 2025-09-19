8be7567cfaee665691790b9dac67de2c0c4add72
Kontravers <rasterman.vector@gmail.com>
2025-09-19 21:51:08 +0200

    reworking the shape

--- src/GeneratedComponent.tsx
+++ src/GeneratedComponent.tsx
@@ -4,8 +4,8 @@ import { PulsingBorder } from '@paper-design/shaders-react';
 /**
  * Code exported from Paper
  * https://app.paper.design/file/01K53FWV94T9HMTSF6T72BC5BN?node=01K5HJJGT050FGH5RRQDW1RKJ5
- * on Sep 19, 2025 at 9:46 PM.
+ * on Sep 19, 2025 at 9:50 PM.
  */
 export default function GeneratedComponent() {
-  return <PulsingBorder colors={['#0dc1fd', '#d915ef', '#ff3f2ecc']} colorBack="#00000000" speed={1.17} roundness={0} thickness={0} softness={0} intensity={0.37} bloom={0.97} spots={2} spotSize={0.78} pulse={0.42} smoke={0.57} smokeSize={0} scale={1} rotation={0} style={{ borderRadius: '0px', height: '130px', width: '531px' }} />;
+  return <PulsingBorder colors={['#20A9F1', '#838CD8', '#DE79BC']} colorBack="#00000000" speed={0.25} roundness={0.75} thickness={0} softness={0} intensity={1} bloom={0.04} spots={2} spotSize={0.78} pulse={0.33} smoke={0.49} smokeSize={0.9} scale={1} rotation={0} style={{ borderRadius: '53px', height: '130px', width: '531px' }} />;
 }
